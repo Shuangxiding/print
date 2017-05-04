e2fc34ebbdb7dc5aea93e98720428651a21acddb
chenjiao <jiao.chen@fintecher.cn>
2017-05-04 18:48:43 +0800

    update

---
+++ 04web/print-web/src/js/input/model_2_input.js
@@ -0,0 +1,14 @@
+   function m2_confirmAndPreview() {
+       console.log("m2_confirmAndPreview");
+       $("#m2_noticenum_span").text($("#memo_number").val());
+       $("#m2_caseNumber_span").text($("#model_casenum").val());
+       $("#m2_caseReason_span").text($("#model_reason").val());
+       $("#m2_borrower_span").text($("#model2_borrower").val());
+       $("#m2_address_span").text($("#model_address").val());
+       $("#m2_sum_span").text($("#model_money").val());
+       $("#m2_contrat_span").text($("#model_contrat").val());
+       $("#m2_phone_span").text($("#model_phone").val());
+       $("#m2_arrivalsYear_span").text($("#model_maturity").val().split("/")[2]);
+       $("#m2_arrivalsMonth_span").text($("#model_maturity").val().split("/")[0]);
+       $("#m2_arrivalsDay_span").text($("#model_maturity").val().split("/")[1]);
+   }
--- 04web/print-web/src/js/model/model_2.js
+++ 04web/print-web/src/js/model/model_2.js
@@ -16,3 +16,43 @@ function preview1() {
     localStorage["callDay"] = $("#callDay").val();
     window.location.href = "../model/model_2.html";
 }
+
+function m2_preview() {
+    $("#m2_num").addClass("opa");
+    $("#m2_tle").addClass("opa");
+    $("#m2_count").addClass("opa");
+    $("#m2_why").addClass("opa");
+    $("#m2_person").addClass("opa");
+    $("#m2_place").addClass("opa");
+    $("#m2_load").addClass("opa");
+    $("#m2_money").addClass("opa");
+    $("#m2_search").addClass("opa");
+    $("#m2_people").addClass("opa");
+    $("#m2_mobail").addClass("opa");
+    $("#m2_date").addClass("opa");
+    $("#m2_seal_data").addClass("opa");
+    $("#m2_sea_content").addClass("opa");
+    $("#m2_tab").addClass("opa");
+    $("#m2_caseNumber_span").addClass("datas");
+    $("#m2_caseReason_span").addClass("datas");
+    $("#m2_borrower_span").addClass("datas");
+    $("#m2_address_span").addClass("datas");
+    $("#m2_arrivalsYear_span").addClass("datas");
+    $("#m2_arrivalsMonth_span").addClass("datas");
+    $("#m2_arrivalsDay_span").addClass("datas");
+    $("#m2_sum_span").addClass("datas");
+    $("#m2_contrat_span").addClass("datas");
+    $("#m2_phone_span").addClass("datas");
+    $("#m2_sum_span").addClass("datas");
+    $(".m2").printArea();
+}
+
+function m2_preview_all() {
+    // $("#m2_tle").addClass("real");
+    // $("#m2_not").addClass("real");
+    // $("#m2_num").addClass("real");
+    // $("#m2_con").addClass("real");
+    // $("#m2_tab tr td").addClass("real");
+    $(".m2").printArea();
+
+}
