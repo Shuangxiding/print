03ca07b8a242caa88a70b8d8a5673b74ee0abc13
wuzhanhui <zhanhui.wu@fintecher.cn>
2017-05-05 11:24:32 +0800

    添加金钱符号

--- 04web/print-web/src/js/input/model_3_input.js
+++ 04web/print-web/src/js/input/model_3_input.js
@@ -5,6 +5,9 @@ function m3_print() {
 
 // 点击确认并预览按钮事件
 function m3_confirmAndPreview() {
+    var m3_money = $("#model_loadamount").prev().text() + $("#model_loadamount").val() + $("#model_loadamount").next().text();
+    var m3_capital = $("#model_principal").prev().text() + $("#model_principal").val() + $("#model_principal").next().text();
+    var m3_interest = $("#model_interest").prev().text() + $("#model_interest").val() + $("#model_interest").next().text();
     $("#m3_ordernum_span").text($("#memo_number").val());
     $("#m3_borrower1").text($("#model_borrower").val());
     $("#m3_surety1").text($("#model_guarantor").val());
@@ -12,14 +15,14 @@ function m3_confirmAndPreview() {
     $("#m3_borrowYear").text($("#model_loandate").val().split("/")[2]);
     $("#m3_borrowMonth").text($("#model_loandate").val().split("/")[1]);
     $("#m3_borrowDay").text($("#model_loandate").val().split("/")[0]);
-    $("#m3_money").text($("#model_loadamount").val());
+    $("#m3_money").text(m3_money);
     $("#m3_application").text($("#model_loaduse").val());
     $("#m3_surety2").text($("#model_guarantor").val());
     $("#m3_dueYear").text($("#model_maturity").val().split("/")[2]);
     $("#m3_dueMonth").text($("#model_maturity").val().split("/")[1]);
     $("#m3_dueDay").text($("#model_maturity").val().split("/")[0]);
-    $("#m3_capital").text($("#model_principal").val());
-    $("#m3_interest").text($("#model_interest").val());
+    $("#m3_capital").text(m3_capital);
+    $("#m3_interest").text(m3_interest);
 
     // 显示打印数据按钮
     $("#m3_printdata_button").removeClass("m3_button_hide");
