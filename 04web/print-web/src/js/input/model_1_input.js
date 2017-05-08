ec3ccee9d9e29f849ce849b529292bf978ef1287
sunyanping <sunyanping24@sina.com>
2017-05-08 19:36:11 +0800

    优化js代码

--- 04web/print-web/src/js/input/model_1_input.js
+++ 04web/print-web/src/js/input/model_1_input.js
@@ -14,24 +14,29 @@ function m1_print() {
 
 // 点击确认并预览按钮事件
 function m1_confirmAndPreview() {
-    var memo_number = $("#memo_number").val(); // 通知单编号
-    var model_borrower = $("#model_borrower").val(); // 借款单位/个人名称
-    var model_loandate = $("#model_loandate").val(); // 贷款日期
-
-    var model_loadamount_symbol = $("#model_loadamount").prev().text(); // 贷款金额金钱符号
-    var model_loadamount = $("#model_loadamount").val(); // 贷款金额
-    var model_loadamount_yuan = $("#model_loadamount").next().text(); // 贷款金额“元”
-
-    var model_maturity = $("#model_maturity").val(); // 贷款到期日期
-
-    var model_principal_symbol = $("#model_principal").prev().text(); // 贷款金额金钱符号
-    var model_principal = $("#model_principal").val(); // 至今欠款本金
-    var model_principal_yuan = $("#model_principal").next().text(); // 贷款金额“元”
-
-    var model_interest_symbol = $("#model_interest").prev().text(); // 贷款金额金钱符号
-    var model_interest = $("#model_interest").val(); // 至今欠款利息
-    var model_interest_yuan = $("#model_interest").next().text(); // 贷款金额“元”
-
+    // 通知单编号
+    var memo_number = $("#memo_number").val();
+    // 借款单位/个人名称
+    var model_borrower = $("#model_borrower").val();
+    // 贷款日期
+    var model_loandate = $("#model_loandate").val();
+    var date = model_loandate.split("/");
+    // 贷款金额
+    var model_loadamount_symbol = $("#model_loadamount").prev().text();
+    var model_loadamount = $("#model_loadamount").val();
+    var model_loadamount_yuan = $("#model_loadamount").next().text();
+    // 贷款到期日期
+    var model_maturity = $("#model_maturity").val();
+    var date1 = model_maturity.split("/");
+    // 至今欠款本金
+    var model_principal_symbol = $("#model_principal").prev().text();
+    var model_principal = $("#model_principal").val();
+    var model_principal_yuan = $("#model_principal").next().text();
+    // 至今欠款利息
+    var model_interest_symbol = $("#model_interest").prev().text();
+    var model_interest = $("#model_interest").val();
+    var model_interest_yuan = $("#model_interest").next().text();
+    // 简单判断，数据为空时不能预览（后面做修改）
     if (memo_number === null || memo_number === '' ||
         model_borrower === null || model_borrower === '' ||
         model_loandate === null || model_loandate === '' ||
@@ -41,31 +46,21 @@ function m1_confirmAndPreview() {
         model_interest === null || model_interest === '') {
         alert("录入信息不能存在空值！");
         return;
-
     }
     // 添加关闭此窗口属性
     $("#m1_button_preview").attr("data-dismiss", "modal");
-
+    // 填值
     $("#m1_noticenum_span").text(memo_number);
-
     $("#m1_name").text(model_borrower);
-
-    var date = model_loandate.split("/");
     $("#m1_byear").text(date[2]);
     $("#m1_bmonth").text(date[1]);
     $("#m1_bday").text(date[0]);
-
     $("#m1_bmoney").text(model_loadamount_symbol + model_loadamount + model_loadamount_yuan);
-
-    var date1 = model_maturity.split("/");
     $("#m1_cyear").text(date1[2]);
     $("#m1_cmonth").text(date1[1]);
     $("#m1_cday").text(date1[0]);
-
     $("#m1_omoney").text(model_principal_symbol + model_principal + model_principal_yuan);
-
     $("#m1_ointerest").text(model_interest_symbol + model_interest + model_interest_yuan);
-
     // 显示打印数据按钮
     $("#m1_printdata_button").removeClass("m1_bttton_hide");
 }
