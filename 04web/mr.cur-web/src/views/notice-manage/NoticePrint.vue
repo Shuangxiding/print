364baebf6d27ea33c4b37a1ae3c5357da82a93b1
chenjiao <jiao.chen@fintecher.cn>
2017-05-15 15:40:53 +0800

    解决冲突

--- 04web/mr.cur-web/src/views/notice-manage/NoticePrint.vue
+++ 04web/mr.cur-web/src/views/notice-manage/NoticePrint.vue
@@ -1,5 +1,4 @@
 <template>
-<<<<<<< HEAD
   <div class="page">
     <el-tabs v-model="activeName" @tab-click="handleClick">
       <el-tab-pane label="通知单模板一" name="first">
@@ -33,35 +32,10 @@
   import NoticeTempOne from '@/views/notice-manage/NoticeTempOne'
   import NoticeTempTwo from '@/views/notice-manage/NoticeTempTwo'
   import NoticeTempThree from '@/views/notice-manage/NoticeTempThree'
-=======
-  <el-col>
-    <el-col :span="5">
-      <div class="header">请选择贷后催收通知单模板:</div>
-    </el-col>
-    <el-col class="tab-box">
-      <el-tabs type="card">
-        <el-tab-pane label="模板一" class="tab-pane" onclick="first">
-        </el-tab-pane>
-        <el-tab-pane label="模板二" class="tab-pane" onclick="second">
-        </el-tab-pane>
-        <el-tab-pane label="模板三" class="tab-pane" onclick="third">
-        </el-tab-pane>
-      </el-tabs>
-    </el-col>
-    <div class="btns">
-      <el-button>打印</el-button>
-      <el-button>打印数据</el-button>
-    </div>
-  </el-col>
-</template>
-<script>
-  import TemplateFirst from '@/views/notice-manage/TemplateFirst'
->>>>>>> 76a7122b2774b6938b91fedf1df0487e56cbf759
   export default {
-    name: 'print',
+    name: 'notice-print',
     data() {
       return {
-<<<<<<< HEAD
         activeName: 'second'
       }
     },
@@ -74,26 +48,11 @@
       NoticeTempOne,
       NoticeTempTwo,
       NoticeTempThree
-=======
-
-      }
-    },
-    methods: {
-      first() {
-        console.log('first')
-      }
-    },
-    components: {
-      TemplateFirst
->>>>>>> 76a7122b2774b6938b91fedf1df0487e56cbf759
     }
   }
 
 </script>
-<<<<<<< HEAD
 <style scoped>
 
 
 </style>
-=======
->>>>>>> 76a7122b2774b6938b91fedf1df0487e56cbf759
