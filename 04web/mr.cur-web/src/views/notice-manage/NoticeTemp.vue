576e79d348a36787207813e3554634435b93e1aa
sunyanping <sunyanping24@sina.com>
2017-05-15 11:23:38 +0800

    NoticeTemp.vue修改

--- 04web/mr.cur-web/src/views/notice-manage/NoticeTemp.vue
+++ 04web/mr.cur-web/src/views/notice-manage/NoticeTemp.vue
@@ -1,10 +1,25 @@
 <template>
   <div class="page">
+    <el-tabs v-model="activeName" @tab-click="handleClick">
+      <el-tab-pane label="通知单模板一" name="first">通知单模板一展示区</el-tab-pane>
+      <el-tab-pane label="通知单模板二" name="second">通知单模板二展示区</el-tab-pane>
+      <el-tab-pane label="通知单模板三" name="third">通知单模板三展示区</el-tab-pane>
+    </el-tabs>
   </div>
 </template>
 <script>
   export default {
-    name: 'notice-temp'
+    name: 'notice-temp',
+    data() {
+      return {
+        activeName: 'second'
+      }
+    },
+    methods: {
+      handleClick(tab, event) {
+        console.log(tab, event)
+      }
+    }
   }
 
 </script>
