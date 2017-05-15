bdbf3ca248b1f037bcf8572fb65d695f2a9a9b47
sunyanping <sunyanping24@sina.com>
2017-05-15 15:25:50 +0800

    ...

--- 04web/mr.cur-web/src/views/notice-manage/NoticePrint.vue
+++ 04web/mr.cur-web/src/views/notice-manage/NoticePrint.vue
@@ -1,52 +1,58 @@
 <template>
-  <el-col>
-    <el-col>
-      <div class="header">请选择贷后催收通知单模板:</div>
-    </el-col>
-    <el-col class="tab-box">
-      <el-tabs>
-        <el-tab-pane label="贷后催收通知单模板" class="tab-pane">
-          <div class="tab-pane">
-            <el-form label-width="125px">
-              <el-form-item label="催收通知单模板名称:">
-                <el-select v-model="templateClass" placeholder="请选择模板">
-                  <el-option v-for="item in this.templateClass" :value="item"></el-option>
-                </el-select>
-              </el-form-item>
-            </el-form>
-          </div>
-        </el-tab-pane>
-      </el-tabs>
-    </el-col>
-    <div>预览区</div>
-    <div class="btns">
-      <el-button>预览</el-button>
-      <el-button>打印</el-button>
-      <el-button>打印数据</el-button>
-    </div>
-  </el-col>
+  <div class="page">
+    <el-tabs v-model="activeName" @tab-click="handleClick">
+      <el-tab-pane label="通知单模板一" name="first">
+        <notice-temp-one></notice-temp-one>
+        <div class="btns">
+          <el-button>预览</el-button>
+          <el-button>打印</el-button>
+          <el-button>打印数据</el-button>
+        </div>
+      </el-tab-pane>
+      <el-tab-pane label="通知单模板二" name="second">
+        <notice-temp-two></notice-temp-two>
+        <div class="btns">
+          <el-button>预览</el-button>
+          <el-button>打印</el-button>
+          <el-button>打印数据</el-button>
+        </div>
+      </el-tab-pane>
+      <el-tab-pane label="通知单模板三" name="third">
+        <notice-temp-three></notice-temp-three>
+        <div class="btns">
+          <el-button>预览</el-button>
+          <el-button>打印</el-button>
+          <el-button>打印数据</el-button>
+        </div>
+      </el-tab-pane>
+    </el-tabs>
+  </div>
 </template>
 <script>
+  import NoticeTempOne from '@/views/notice-manage/NoticeTempOne'
+  import NoticeTempTwo from '@/views/notice-manage/NoticeTempTwo'
+  import NoticeTempThree from '@/views/notice-manage/NoticeTempThree'
   export default {
     name: 'notice-print',
     data() {
       return {
-        templateClass: { 1: '一', 2: '二' }
+        activeName: 'second'
       }
+    },
+    methods: {
+      handleClick(tab, event) {
+        console.log(tab, event)
+      }
+    },
+    components: {
+      NoticeTempOne,
+      NoticeTempTwo,
+      NoticeTempThree
     }
   }
 
 </script>
 <style scoped>
-  .header {
-    margin: 30px 0 30px;
-    font-size: 0.875rem;
-    color: rgb(31, 45, 61);
-    text-align: left;
-  }
 
-  .tab-box {
-    text-align: left;
-  }
 
 </style>
