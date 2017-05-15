f685e0aa37ef4db3ec86308e1822a0a4c7a8ae6d
sunyanping <sunyanping24@sina.com>
2017-05-15 15:03:28 +0800

    增加模板组件

--- 04web/mr.cur-web/src/views/notice-manage/NoticePrint.vue
+++ 04web/mr.cur-web/src/views/notice-manage/NoticePrint.vue
@@ -4,14 +4,13 @@
       <div class="header">请选择贷后催收通知单模板:</div>
     </el-col>
     <el-col class="tab-box">
-      <el-tabs type="card">
+      <el-tabs>
         <el-tab-pane label="贷后催收通知单模板" class="tab-pane">
           <div class="tab-pane">
-            <el-form label-width="120px">
+            <el-form label-width="125px">
               <el-form-item label="催收通知单模板名称:">
-                <el-select>
-                  <el-option>
-                  </el-option>
+                <el-select v-model="templateClass" placeholder="请选择模板">
+                  <el-option v-for="item in this.templateClass" :value="item"></el-option>
                 </el-select>
               </el-form-item>
             </el-form>
@@ -19,6 +18,7 @@
         </el-tab-pane>
       </el-tabs>
     </el-col>
+    <div>预览区</div>
     <div class="btns">
       <el-button>预览</el-button>
       <el-button>打印</el-button>
@@ -31,9 +31,22 @@
     name: 'notice-print',
     data() {
       return {
-
+        templateClass: { 1: '一', 2: '二' }
       }
     }
   }
 
 </script>
+<style scoped>
+  .header {
+    margin: 30px 0 30px;
+    font-size: 0.875rem;
+    color: rgb(31, 45, 61);
+    text-align: left;
+  }
+
+  .tab-box {
+    text-align: left;
+  }
+
+</style>
---
+++ 04web/mr.cur-web/src/views/notice-manage/NoticeTempTwo.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="page">
+    模板二
+  </div>
+</template>
