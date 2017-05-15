00909299c3a698439fb4b0f509735dbc90eef1da
chenjiao <jiao.chen@fintecher.cn>
2017-05-15 15:12:04 +0800

    修改弹出框

--- 04web/mr.cur-web/src/views/notice-manage/NoticePrint.vue
+++ 04web/mr.cur-web/src/views/notice-manage/NoticePrint.vue
@@ -1,28 +1,41 @@
 <template>
   <el-col>
-    <el-col>
+    <el-col :span="5">
       <div class="header">请选择贷后催收通知单模板:</div>
     </el-col>
     <el-col class="tab-box">
       <el-tabs type="card">
-        <el-tab-pane label="贷后催收通知单模板" class="tab-pane">
-          <div class="tab-pane">
-            <el-form label-width="120px">
-              <el-form-item label="催收通知单模板名称:">
-                <el-select>
-                  <el-option>
-                  </el-option>
-                </el-select>
-              </el-form-item>
-            </el-form>
-          </div>
+        <el-tab-pane label="模板一" class="tab-pane" onclick="first">
+        </el-tab-pane>
+        <el-tab-pane label="模板二" class="tab-pane" onclick="second">
+        </el-tab-pane>
+        <el-tab-pane label="模板三" class="tab-pane" onclick="third">
         </el-tab-pane>
       </el-tabs>
     </el-col>
     <div class="btns">
-      <el-button>预览</el-button>
       <el-button>打印</el-button>
       <el-button>打印数据</el-button>
     </div>
   </el-col>
 </template>
+<script>
+  import TemplateFirst from '@/views/notice-manage/TemplateFirst'
+  export default {
+    name: 'print',
+    data() {
+      return {
+
+      }
+    },
+    methods: {
+      first() {
+        console.log('first')
+      }
+    },
+    components: {
+      TemplateFirst
+    }
+  }
+
+</script>
